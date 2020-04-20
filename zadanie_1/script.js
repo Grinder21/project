4bec2398aac1fd3cb7f6619cf34ce125618bceea
Mikhail <spartawinner280@mail.ru>
2020-04-20 10:03:13 +0300

    задание3

--- zadanie_1/script.js
+++ zadanie_1/script.js
@@ -1,17 +1,20 @@
 'use strict';
+
+
 let money, time;
+
 function start() {
-    money = +prompt('Ваш бюджет на месяц?', '');
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    money = +prompt ("Ваш бюджет на месяц?", "");
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
 
-    while(isNaN(money) || money == '' || money == null) {
-        money = +prompt('Ваш бюджет на месяц?', '');
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt ("Ваш бюджет на месяц?", ""); 
     }
-}
 
+}
 start();
 
-
+    
 let appData = {
     budget: money,
     timeData: time,
@@ -21,62 +24,65 @@ let appData = {
     savings: true
 };
 
-let optionalExpenses = {
-    question_1 : 1,
-    question_2 : 2,
-    question_3 : 3
-    }
 
 function chooseExpenses() {
     for (let i = 0; i < 2; i++) {
-        let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-        let b = prompt('Во сколько обойдется?', '');
+        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt ("Во сколько обойдется?", "");
     
-        if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
-        && a !='' && b !='' && a.length < 50) {
-            console.log('done');
+        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
             appData.expenses[a] = b;
         } else {
-            i = i -1;
+            i--;
         }
-    }
+    
+    };
 }
 chooseExpenses();
 
-function detectDayBudget() {
-appData.moneyPerDay = (appData.budget / 30).toFixed();
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+function detectDayBudget() {                                        
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
 }
 detectDayBudget();
 
 
-function detectLevel() {
-   if (appData.moneyPerDay < 100) {
-    console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Средний уровень достатка!');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('Высокий уровень достатка!');
-} else {
-    console.log('Произошла ошибка!');
-} 
+function detectLevel() { 
+    if (appData.moneyPerDay < 100) {
+        console.log ("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log ("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log ("Это высокий уровень достатка!");
+    } else {
+        console.log ("Ошибочка...!");
+    }
 }
 detectLevel();
 
-function checkSaving() {
-    if (appData.savings == true) {
-        let save = +prompt('Какова сумма накоплений?');
-        let percent = +prompt('Под какой процент?');
 
-        appData.monthIncome = save/100/12*percent;
-        alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+            appData.monthIncome = save/100/12*percent;
+            alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
     }
 }
-checkSaving();
-function chooseOptExpenses() {
-    optionalExpenses.question_1 = prompt('Статья необязательных расходов?');
-    optionalExpenses.question_2 = prompt('Статья необязательных расходов?');
-    optionalExpenses.question_3 = prompt('Статья необязательных расходов?');
+checkSavings();
+
+
+function chooseOptExpenses() { 
+
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+        console.log(appData.optionalExpenses);
+    }
+
 
 }
 chooseOptExpenses();
