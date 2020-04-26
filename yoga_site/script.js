6ab31609c3506360430f9a6cf3d37b674e49fce1
Mikhail <spartawinner280@mail.ru>
2020-04-26 14:22:54 +0300

    Добавление tabs + modal window

--- yoga_site/script.js
+++ yoga_site/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
+
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
@@ -10,6 +11,7 @@ window.addEventListener('DOMContentLoaded', function() {
             tabContent[i].classList.add('hide');
         }
     }
+
     hideTabContent(1);
 
     function showTabContent(b) {
@@ -22,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function() {
     info.addEventListener('click', function(event) {
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
-            for (let i = 0; i<tab.length; i++) {
+            for(let i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
@@ -30,22 +32,23 @@ window.addEventListener('DOMContentLoaded', function() {
                 }
             }
         }
-    })
 
-    // Таймер
+    });
 
-    let deadline = '2020-08-31';
+    // Timer 
+
+    let deadline = '2018-11-21';
 
     function getTimeRemaining(endtime) {
-        let t = Date.parse(endtime) - Date.parse(new Date());
-        let seconds = Math.floor((t/1000) % 60),
-            minutes = Math.floor((t/1000/60) % 60),
-            hours = Math.floor((t/(1000*60*60)));
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
 
         return {
             'total' : t,
             'hours' : hours,
-            'minutes': minutes,
+            'minutes' : minutes,
             'seconds' : seconds
         };
     }
@@ -54,21 +57,21 @@ window.addEventListener('DOMContentLoaded', function() {
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds');
-            let timeInterval = setInterval(updateClock, 1000);
-
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
         function updateClock() {
             let t = getTimeRemaining(endtime);
 
-        function addZero(num){
-                if(num <= 9) {
-                    return '0' + num;
-                } else return num;
-            };
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
 
-    hours.textContent = addZero(t.hours);
-    minutes.textContent = addZero(t.minutes);
-    seconds.textContent = addZero(t.seconds);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
@@ -77,7 +80,44 @@ window.addEventListener('DOMContentLoaded', function() {
                 seconds.textContent = '00';
             }
         }
+
     }
 
     setClock('timer', deadline);
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        tabs = document.querySelectorAll('.description-btn'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    tabs.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
 });
+
+// Второе задание
+
+// let age = document.getElementById('age');
+ 
+// function showUser(surname, name) {
+//          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+ 
+// showUser.apply(age, ["Горький","Максим"]);
--- zadanie_1/test-script.js
+++ zadanie_1/test-script.js
@@ -1,17 +1,71 @@
-let box = document.querySelector('.box');
-let btn = document.querySelector('button');
-let width = box.clientWidth,
-    height = box.clientHeight;
+// function User (name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello!', this.name);
+//     }
+// };
 
-console.log(width);
-console.log(height);
-console.log(box.getBoundingClientRect().left);
+// User.prototype.exit = function(name) {
+//     console.log('Пользователь ' + this.name + ' ушел');
+// }
+// let ivan = new User('Ivan', 25),
+//     alex = new User('Alex', 20);
 
-console.log(document.documentElement.clientWidth);
-console.log(document.documentElement.clientHeight);
+// console.log(ivan);
+// console.log(alex);
+
+// ivan.exit();
+
+// 'use strict';
+// function showThis(a,b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+// showThis(4,5);
+// showThis(5,5);
+
+// let obj = {
+//     a: 20,
+//     b: 15,
+//     sum : function() {
+//         console.log(this);
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+
+// obj.sum();
 
 
-btn = addEventListener('click', function() {
-    box.scrollTop = 0;
-})
+// let user = {
+//     name: 'John'
+// };
 
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// console.log(sayName.call(user, ' Smith'));
+// console.log(sayName.apply(user, [' Snow']));
+
+// function count(number) {
+//     return this* number;
+// }
+// let double = count.bind(2);
+// console.log(double(3));
+// console.log(double(10));
+
+
+// 1) Просто вызов функции - window/undefined
+// 2) Метод объекта - this = объект
+// 3) Конструктор (new) - this = новый созданный объект
+// 4) Указание конкретного контекста - call, apply, bind
