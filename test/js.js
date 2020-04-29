9e17b8257e1369671dc244beb46e6ed38ce1e01b
Mikhail <spartawinner280@mail.ru>
2020-04-29 13:56:33 +0300

    добавление класса и создание блока

--- test/js.js
+++ test/js.js
@@ -1,44 +1,14 @@
-// let timerId = setTimeout(sayHello, 3000);
-// clearTimeout(timerId);
+let video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'bloger'],
+    internet = [...video, ...blogs, 'vk', 'facebook'];
 
-// let timerId = setInterval(sayHello, 3000);
-// clearTimeout(timerId);
+console.log(internet);
 
-// function sayHello() {
-//     console.log('Hello World');
-// }
-
-// let timerId = setTimeout(function log() {
-//     console.log('Hello');
-//     setTimeout(log, 2000);
-// })
-
-let btn = document.querySelector('.btn');
-let elem = document.querySelector('.box');
-
-function myAnimation() {
-    let pos = 0;
-
-    let id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
+function log(a,b,c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(a + b + c);
 }
-
-btn.addEventListener('click', myAnimation);
-
-
-let btnBlock = document.querySelector('.btn-block'),
-    btns = document.getElementsByTagName('button');
-
-    btnBlock.addEventListener('click', function(event) {
-        if(event.target && event.target.tagName == 'BUTTON') {
-            console.log('Hello');
-        }
-    })
+let numbers = [2,5,7];
+log(...numbers);
